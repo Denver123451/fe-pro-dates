228e31b09838adeec4455dd869d0b7c9a27f13f0
Denver12345 <villaan12345>
2022-06-29 20:09:05 +0300

    Done!

--- js/dates.js
+++ js/dates.js
@@ -2,33 +2,22 @@
 // мы передаем объект даты и вторым аргументом язык(доступно либо 'en' либо 'ru')
 // и функция должна вернуть день недели словом в зависимости от языка. Очень круто,
 // если выйдет сделать это ОДНОЙ строкой.
-function getDay(date, lang) {
-  const dayNames = {
-    en: {
-      0: 'Sunday',
-      1: 'Monday',
-      2: 'Tuesday',
-      3: 'Wednesday',
-      4: 'Thursday',
-      5: 'Friday',
-      6: 'Saturday',
-    },
-    ru: {
-      0: 'Воскресенье',
-      1: 'Понедельник',
-      2: 'Вторник',
-      3: 'Среда',
-      4: 'Четверг',
-      5: 'Пятница',
-      6: 'Суббота',
-    },
-  };
-}
+const getDay = (date, lang) =>
+  date
+    .toLocaleString(lang, { weekday: 'long' })
+    .split('')
+    .map((el, id) => (id === 0 ? el.toUpperCase() : el))
+    .join('');
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
-function formatTime(date) {}
+const formatTime = (date) =>
+  date.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
 
 /*
 Напишите функцию getLastDayOfMonth(year, month), 
@@ -39,7 +28,9 @@ year – год из четырёх цифр, например, 2012.
 month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
-function getLastDayOfMonth(year, month) {}
+function getLastDayOfMonth(year, month) {
+  return new Date(year, month + 1, 0).getDate();
+}
 
 module.exports = {
   getDay,
